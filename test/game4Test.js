b62f5d80ee509d123c105210b0154f7109488995
Gouthum Karadi <karadi@virtualnex.us>
2022-02-17 02:26:12 -0400

    Game4 isWon

--- test/game4Test.js
+++ test/game4Test.js
@@ -6,9 +6,21 @@ describe("Game4", function() {
     const game = await Game.deploy();
     await game.deployed();
 
-    // nested mappings are rough :}
+    //Get signers and addresses
+    const signer = ethers.provider.getSigner(0);
+    const address1 = await signer.getAddress();
 
-    await game.win();
+    const signer1 = ethers.provider.getSigner(1);
+    const address2 = await signer1.getAddress();
+
+    const signer2 = ethers.provider.getSigner(2);
+    const address3 = await signer2.getAddress();
+
+    // nested mappings are rough :}
+    await game.write(address1);
+
+
+    await game.win(address1);
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
