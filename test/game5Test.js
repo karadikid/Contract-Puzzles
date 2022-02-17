8a0066d8e5c2c27acc9b5baef5ce1ed994b87c44
Gouthum Karadi <karadi@virtualnex.us>
2022-02-17 03:04:12 -0400

    Completed all but 5 - SemdTx amount bigNumber too small.

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,14 +1,25 @@
 const { assert } = require("chai");
 
-describe("Game5", function() {
-  it("should be a winner", async function() {
+describe("Game5", function () {
+  it("should be a winner", async function () {
     const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
     await game.deployed();
 
-    // good luck
+    //Get signers and addresses
+    const signer = new ethers.Wallet.createRandom();
+    //Traditional 
+    const signer1= ethers.provider.getSigner(1);
+    
+    const sendTx = await signer1.sendTransaction({to: signer.address, value: 10000})
+    receipt = sendTx;
+    console.log(receipt);
 
-    await game.win();
+    // good luck
+    // to call a contract as a signer you can use contract.connect
+    //await game.connect(signer).win();
+
+    await game.connect(signer.connect(ethers.provider)).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
